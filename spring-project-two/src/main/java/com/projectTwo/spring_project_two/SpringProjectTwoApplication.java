0a42a8900c5e0ae5b1fc3865b25785ef25db8f7f
ttsosie <tiffiny.tsosie@mykenzie.snhu.edu>
2025-07-25 02:28:39 -0700

    Created Spring Project and moved into Documents

---
+++ spring-project-two/src/main/java/com/projectTwo/spring_project_two/SpringProjectTwoApplication.java
@@ -0,0 +1,13 @@
+package com.projectTwo.spring_project_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectTwoApplication.class, args);
+	}
+
+}
---
+++ spring-project-two/src/test/java/com/projectTwo/spring_project_two/SpringProjectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectTwo.spring_project_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
