0f71fc5be42fb8ba284d654e0cb820e9229b26bc
ttsosie <tiffiny.tsosie@mykenzie.snhu.edu>
2025-07-31 02:47:10 -0700

    Warehouses Table Repository - re push to github

--- warehouse-api/src/main/java/com/skillSTORM/warehouse_api/repositories/WarehousesRepository.java
+++ warehouse-api/src/main/java/com/skillSTORM/warehouse_api/repositories/WarehousesRepository.java
@@ -1,5 +1,11 @@
 package com.skillSTORM.warehouse_api.repositories;
 
-public interface WarehousesRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.skillSTORM.warehouse_api.models.Warehouses;
+
+@Repository
+public interface WarehousesRepository extends CrudRepository<Warehouses, Integer> {
 
 }
