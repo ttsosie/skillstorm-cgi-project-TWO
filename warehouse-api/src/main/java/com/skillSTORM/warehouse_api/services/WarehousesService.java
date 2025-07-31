7890d78512694b81f91f7f1078a0a256eda60da2
ttsosie <tiffiny.tsosie@mykenzie.snhu.edu>
2025-07-31 02:56:10 -0700

    had to resave Warehouses service file to update with code for the findAll()

--- warehouse-api/src/main/java/com/skillSTORM/warehouse_api/services/WarehousesService.java
+++ warehouse-api/src/main/java/com/skillSTORM/warehouse_api/services/WarehousesService.java
@@ -1,5 +1,19 @@
 package com.skillSTORM.warehouse_api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skillSTORM.warehouse_api.models.Warehouses;
+import com.skillSTORM.warehouse_api.repositories.WarehousesRepository;
+
+@Service
 public class WarehousesService {
+	
+	@Autowired
+	private WarehousesRepository repo;
+	
+	public Iterable<Warehouses> finalAll(){
+		return this.repo.findAll();
+	}
 
 }
