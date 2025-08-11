d39efc590f9a8e1a35c08a901f9665f00b4cdc1f
ttsosie <tiffiny.tsosie@mykenzie.snhu.edu>
2025-08-11 02:59:18 -0700

    Refactoring Customers Service for matching dto

--- warehouse-api/src/main/java/com/skillSTORM/warehouse_api/services/CustomersService.java
+++ warehouse-api/src/main/java/com/skillSTORM/warehouse_api/services/CustomersService.java
@@ -1,5 +1,81 @@
 package com.skillSTORM.warehouse_api.services;
 
-public class CustomersService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.skillSTORM.warehouse_api.dtos.CustomersDTO;
+import com.skillSTORM.warehouse_api.models.Customers;
+import com.skillSTORM.warehouse_api.repositories.CustomersRepository;
+
+
+// Injects the repository to use to run the service methods
+// This IS-A BEAN
+
+@Service
+public class CustomersService {
+private final CustomersRepository repo;
+	
+	// Constructor Injection
+	// Populates the repository variable when Bean is instantiated so repository is final
+	@Autowired
+	public CustomersService(CustomersRepository repo) {
+		this.repo = repo;
+	}
+	
+	// To get back all of the existing Customers
+	public ResponseEntity<Iterable<Customers>> finalAll(){
+		Iterable<Customers> customers = this.repo.findAll();
+		if (!customers.iterator().hasNext())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+								 .body(null);
+		
+		return ResponseEntity.ok(customers);
+	}
+	
+	// To get a specific customer by id
+	public ResponseEntity<Customers> findById(int id){
+		Optional<Customers> customer = this.repo.findById(id);
+		
+		if(customer.isPresent()) { // Using Optional gives the isPresent or isEmpty option from CRUD
+			return ResponseEntity.ok(customer.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	// If user needed to delete a customer by a specific id
+	public ResponseEntity<Void> deleteById(int id){
+		this.repo.deleteById(id);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+	
+	// If user wanted to create a customer - id(don't care about), name, price, details, warehouseId (where it's stored), quantity
+	public ResponseEntity<Customers> createOne(CustomersDTO dto){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(this.repo.save(
+						new Customers(
+						0,
+						dto.firstName(), 
+						dto.lastName(), 
+						dto.email(), 
+						dto.state())));
+	}
+	
+	// If user wanted to update a customer id
+	public ResponseEntity<Customers> updateOne(int id, CustomersDTO dto){
+		if(this.repo.existsById(id)) {
+			return ResponseEntity.ok(this.repo.save(
+					new Customers(
+					0, 
+					dto.firstName(), 
+					dto.lastName(), 
+					dto.email(), 
+					dto.state())));
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
 }
